fa906ba98f7042daca1cc41d45f6202d57d874e2
Maja Hedegard <mh223pi@student.lnu.se>
2021-04-09 16:48:16 +0200

    add authenticateJWT and hasPermission

--- src/routes/api/v1/images-router.js
+++ src/routes/api/v1/images-router.js
@@ -6,29 +6,77 @@
  */
 
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
 import { ImagesController } from '../../../controllers/api/images-controller.js'
 
 export const router = express.Router()
 
 const controller = new ImagesController()
 
+const PermissionLevels = Object.freeze({
+  READ: 1,
+  CREATE: 2,
+  UPDATE: 4,
+  DELETE: 8
+})
+
+/**
+ * Authenticates requests.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+const authenticateJWT = (req, res, next) => {
+  const authorization = req.headers.authorization?.split(' ') // kastar inte undantag om authorization är undefined - behöver inte ha if stats innnan för att se om authorization är satt.
+
+  if (authorization?.[0] !== 'Bearer') { // skapra 401 om authorization är tom, kontrollerar at det är en bearer
+    next(createError(401))
+    return
+  }
+
+  try {
+    req.jwt = jwt.verify(authorization[1], process.env.ACCESS_TOKEN_SECRET) // samma som när vi signerar
+    req.user = {
+      email: req.jwt.email,
+      permissionLevel: req.jwt.permission_level
+    }
+    next()
+  } catch (err) {
+    next(createError(403))
+  }
+}
+
+/**
+ * Authorize requests.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @param {number} permissionLevel - ...
+ */
+const hasPermission = (req, res, next, permissionLevel) => {
+  req.user?.permissionLevel & permissionLevel ? next() : next(createError(403))
+}
+
 // Provide req.image to the route if :id is present in the route path.
-router.param('id', (req, res, next, id) => controller.loadImage(req, res, next, id))
+router.param('id', authenticateJWT, (req, res, next, id) => controller.loadImage(req, res, next, id))
 
 // GET images
-router.get('/', (req, res, next) => controller.findAll(req, res, next))
+router.get('/', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.findAll(req, res, next))
 
 // POST images
-router.post('/', (req, res, next) => controller.create(req, res, next))
+router.post('/', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.create(req, res, next))
 
 // GET images/:id
-router.get('/:id', (req, res, next) => controller.find(req, res, next))
+router.get('/:id', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.find(req, res, next))
 
 // PUT images/:id
-router.put('/:id', (req, res, next) => controller.update(req, res, next))
+router.put('/:id', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.update(req, res, next))
 
 // PATCH images/:id
-router.patch('/:id', (req, res, next) => controller.partiallyUpdate(req, res, next))
+router.patch('/:id', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.partiallyUpdate(req, res, next))
 
 // DELETE tasks/:id
-router.delete('/:id', (req, res, next) => controller.delete(req, res, next))
+router.delete('/:id', authenticateJWT, (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ), (req, res, next) => controller.delete(req, res, next))
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -10,5 +10,5 @@ import { router as imagesRouter } from './images-router.js'
 
 export const router = express.Router()
 
-router.get('/', (req, res) => res.json({ message: 'Hooray! Welcome to version 1 of this very simple RESTful API!' }))
+router.get('/', (req, res) => res.json({ message: 'Hooray! Welcome to version 1 of this very simple RESTful API! (Picture it - resource' }))
 router.use('/images', imagesRouter)
